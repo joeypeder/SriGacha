de193c458a18370adf7bc2294a1b90d20d224c70
Joseph Pederson <mb1332hu@mnstate.edu>
2022-04-27 13:45:39 -0500

    Formatted display, added blank currency service file

--- app/src/main/java/com/example/srirgacha/MainActivity.kt
+++ app/src/main/java/com/example/srirgacha/MainActivity.kt
@@ -50,7 +50,7 @@ class MainActivity : AppCompatActivity() {
     //TODO("move math to MathService")
     fun CalculateOdds(calc: Double, cost: Double): String {
         var compliment = (100 - calc) / 100
-        return "50%: \n$" + round(log(0.5, compliment) * cost) + "\n90%: \n$" + round(log(0.1, compliment) * cost) + "\n99%: \n$" + round(
+        return "50%: $" + round(log(0.5, compliment) * cost) + "\n90%: $" + round(log(0.1, compliment) * cost) + "\n99%: $" + round(
             log(0.01, compliment) * cost)
     }
 
---
+++ app/src/main/java/com/example/srirgacha/service/CurrencyService.kt
@@ -0,0 +1,4 @@
+package com.example.srirgacha.service
+
+class CurrencyService {
+}
